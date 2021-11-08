97d2c4274e3a01243bc37ad9e607f8559bbf3a85
Borba <gabriel.lewis@acad.pucrs.bbr>
2021-11-07 23:02:03 -0300

    server

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ const app = express()
 
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname, 'dist/spa', 'index.html'));
+});
 app.listen(port)
